dc3a67a02a02f311379638220f78d45a46e9face
futao <1185172056@qq.com>
2019-10-11 11:51:11 +0800

    重构-rename from lazyer to lazy

---
+++ lazy-foundation/src/main/java/com/lazyer/foundation/annotation/impl/AssertEnumClassImpl.java
@@ -0,0 +1,65 @@
+package com.lazyer.foundation.annotation.impl;
+
+import com.lazyer.foundation.annotation.AssertEnumClass;
+import com.lazyer.foundation.foundation.exception.ApplicationException;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.internal.engine.constraintvalidation.ConstraintValidatorContextImpl;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * 枚举值类校验
+ *
+ * @author futao
+ * Created on 2019/9/23.
+ */
+@Slf4j
+public class AssertEnumClassImpl implements ConstraintValidator<AssertEnumClass, Object> {
+
+    private static final Map<String, Integer[]> CACHE = new HashMap<>();
+
+    @Override
+    public void initialize(AssertEnumClass constraintAnnotation) {
+        Class enumClass = constraintAnnotation.value();
+        Object[] enumConstants = enumClass.getEnumConstants();
+        Integer[] cachedValue = CACHE.get(enumClass.getName());
+        if (cachedValue == null) {
+            //未缓存，需要进行缓存
+            Integer[] toSaveValue = new Integer[enumConstants.length];
+            CACHE.put(enumClass.getName(), toSaveValue);
+            for (int i = 0; i < enumConstants.length; i++) {
+                try {
+                    Object enumConstant = enumConstants[i];
+                    Method method = enumClass.getMethod("getType");
+                    Object invoke = method.invoke(enumConstant);
+                    toSaveValue[i] = (int) invoke;
+                } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                    throw ApplicationException.ae("枚举值%s需要实现IEnum接口", enumClass.getName());
+                }
+            }
+        }
+        log.info("手写的枚举值校验初始化完成");
+    }
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        AssertEnumClass assertEnumClass = (AssertEnumClass) ((ConstraintValidatorContextImpl) context).getConstraintDescriptor().getAnnotation();
+        if (assertEnumClass.nullable() && value == null) {
+            return true;
+        }
+        String name = assertEnumClass.value().getName();
+        Integer[] enumValues = CACHE.get(name);
+        for (Integer enumValue : enumValues) {
+            if (Objects.equals(enumValue, value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ lazy-http-client/src/test/java/com/lazyer/httpclient/DeleteRequestTest.java
@@ -0,0 +1,28 @@
+package com.lazyer.httpclient;
+
+import org.junit.Test;
+
+/**
+ * @author futao
+ * Created on 2019-07-05.
+ */
+public class DeleteRequestTest {
+
+    @Test
+    public void send() {
+//        PutRequest request = new PutRequest("https://ec-service-dev.byton.cn/api-user/v1/cms/admin-user/login");
+//        request.addEntity("{\"loginId\":\"admin\",\"password\":\"e10adc3949ba59abbe56e057f20f883e\",\"code\":\"4nnx\",\"loginType\":1,\"state\":1,\"verifyKey\":\"b14570e2acaa43f2981962dbfd878a34\"}");
+//        request.send();
+
+//        GetRequest request = new GetRequest("https://ec-service-dev.byton.cn/api-payment/v1/cms/payments?orderNo=&startTime=&endTime=&tradeType=&status=&page=1&perPage=15");
+//        GetRequest request = new GetRequest("https://ec-service-dev.byton.cn/api-payment/v1/cms/allin-balance");
+        GetRequest request = new GetRequest("https://ec-service-dev.byton.cn/api-dict/v1/cms/setting/?name=system");
+        request.addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhZG1pbiI6dHJ1ZSwiZXhwIjoxNTYyNTc0OTUyLCJ1c2VyaWQiOiI1YzZiYmI2NmExODkwZTlmYWY5Mjc5MGUifQ.uHXywUVSQR_jB2wUSmg_Di447DATVqgFMdOIRgH-TkM");
+        request.addHeader("Accept", "*/*");
+//        request.addUserAgent(UserAgentEnum.CHROME);
+//        request.addHeader("Accept-Language", "zh");
+//        request.addHttpEntity("123");
+        request.send();
+
+    }
+}
