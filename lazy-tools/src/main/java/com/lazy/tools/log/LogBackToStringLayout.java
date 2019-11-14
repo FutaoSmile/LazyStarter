57829df43ddde0ec58d7e40cfdd36162b74639b9
futao <1185172056@qq.com>
2019-11-14 20:54:43 +0800

    对打印的日志对象进行JSONString转化

--- lazy-tools/src/main/java/com/lazy/tools/log/LogBackToStringLayout.java
+++ lazy-tools/src/main/java/com/lazy/tools/log/LogBackToStringLayout.java
@@ -4,6 +4,7 @@ import ch.qos.logback.classic.pattern.MessageConverter;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.helpers.MessageFormatter;
 
 import java.util.stream.Stream;
@@ -17,10 +18,14 @@ public class LogBackToStringLayout extends MessageConverter {
     @Override
     public String convert(ILoggingEvent event) {
         try {
-            return MessageFormatter.arrayFormat(event.getMessage(), Stream.of(event.getArgumentArray()).map(JSON::toJSONString).toArray()).getMessage();
+            Object[] argumentArray = event.getArgumentArray();
+            if (ArrayUtils.isEmpty(argumentArray)) {
+                return event.getMessage();
+            }
+            return MessageFormatter.arrayFormat(event.getMessage(), Stream.of(argumentArray).map(JSON::toJSONString).toArray()).getMessage();
         } catch (Exception e) {
             log.error("toString时发生异常", e);
-            return e.getMessage();
+            return event.getMessage();
         }
     }
 }
